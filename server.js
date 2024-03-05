ea696dc19e77ca330c6ab06ccf29450413ee5180
briangor <briangor13@gmail.com>
2024-03-05 21:13:02 +0300

    update cors options (whitelist)

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = express();
 
 dotenv.config();
 
-var corsOptions = {
-    origin: "http://127.0.0.1:8081"
+let corsOptions = {
+    origin: ["http://127.0.0.1:8081", "http://localhost:3000", process.env.PROD_URL]
 };
 
 app.use(cors(corsOptions));
